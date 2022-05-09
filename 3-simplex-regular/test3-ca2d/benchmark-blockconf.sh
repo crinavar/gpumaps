308cc3f925a4097352a1ba2392b0405c5f4e6023
Felipe Quezada <kezada66@gmail.com>
2022-05-09 16:51:07 -0400

    [simplex3d-reg] Changed benchmarks scripts

--- 3-simplex-regular/test3-ca2d/benchmark-blockconf.sh
+++ 3-simplex-regular/test3-ca2d/benchmark-blockconf.sh
@@ -1,65 +1,76 @@
 #!/bin/bash
-if [ "$#" -ne 11 ]; then
-    echo "run as ./benchmark-blockconf.sh    DEV     STARTB ENDB DB    STARTN ENDN DN     KREPEATS SAMPLES BINARY OUTFILE"
+if [ "$#" -ne 10 ]; then
+    echo "run as ./benchmark-blockconf.sh DEV GPU_ID ARCH    STARTB ENDB DB    STARTN ENDN DN     OUTTER_REPEATS"
     exit;
 fi
 DEV=$1
-STARTB=$2
-ENDB=$3
-DB=$4
-STARTN=$5
-ENDN=$6
-DN=$7
-R=$8
-SAMPLES=$9
-BINARY=${10}
-OUTFILE=${11}
-for B in `seq ${STARTB} ${DB} ${ENDB}`;
+GPUID=$2
+ARCH=$3
+STARTB=$4
+ENDB=$5
+DB=$6
+STARTN=$7
+ENDN=$8
+DN=$9
+R=${10}
+
+METHODS=("BBox" "Hadouken" "DP")
+NM=$((${#METHODS[@]}))
+
+echo "REPEATS=${R}"
+GPUPROG=./bin/prog
+DATE=$(exec date +"%T-%m-%d-%Y (%:z %Z)")
+echo "DATE = ${DATE}"
+
+
+for BP in `seq ${STARTB} ${DB} ${ENDB}`;
 do
-    echo "Benchmarking for B=${B}"
-    echo "Compiling with BSIZE3D=$B"
-    COMPILE=`make BSIZE3D=${B}`
-    echo ${COMPILE}
+    echo "Benchmarking for BPOWER=${BP} METHODS=${NM}"
+    BS=$((2**${BP}))
+    LB=$((${BS} * ${BS}))
+	OUTPUT=data/${GPUID}-REP${R}-BS${BP}.dat
+    cont=0
     for N in `seq ${STARTN} ${DN} ${ENDN}`;
     do
-        echo "B=${B}            DEV=${DEV}  N=${N} R=${R}"
-
-        echo -n "BB  map (${SAMPLES} Samples)............."
-        M=0
-        S=0
-        for k in `seq 1 ${SAMPLES}`;
+        REP=100
+        echo "GPU=${GPUID}  N=${N} B=${BP} R=${N}"
+        echo -n "${N}   ${BP}    " >> ${OUTPUT}
+        for q in `seq 1 ${NM}`;
         do
-            x=`./${BINARY} ${DEV} ${N} ${R} 0`
-            oldM=$M;
-            M=$(echo "scale=10;  $M+($x-$M)/$k"           | bc)
-            S=$(echo "scale=10;  $S+($x-$M)*($x-${oldM})" | bc)
+            # Chosen MAP
+            if [ $q -eq 2 ]
+            then
+                COMPILE=`make BSIZE3DX=${BS} BSIZE3DY=${BS} BSIZE3DZ=${BS} ARCH=${ARCH} DP=SI`
+                echo "Compiling with BSIZE2D=$BS RLEVEL=$N"
+                echo ${COMPILE}
+            else
+                COMPILE=`make BSIZE3DX=${BS} BSIZE3DY=${BS} BSIZE3DZ=${BS} ARCH=${ARCH}`
+                echo "Compiling with BSIZE2D=$BS RLEVEL=$N"
+                echo ${COMPILE}
+            fi
+            
+            echo "${GPUPROG} ${DEV} ${N} ${REP} 0.5 1 ${q}"
+            #echo "${GPUPROG} ${DEV} ${REP} 2 0.5 1"
+            echo -n "[BENCHMARK] ${METHODS[$(($q-1))]} ($q) map (${SAMPLES} Samples)............."
+			x=`${GPUPROG} ${DEV} ${N} ${REP} 0.5 1 ${q}`
+            echo "done"
+            echo "---> BPOWER=${BP} RLEVEL=${N} --> (MEAN, VAR, STDEV, STERR) -> (${x})"
+			echo "Saving in ${OUTPUT}..."
+            echo -n "${x}    " >> "${OUTPUT}"
+			echo "done"
         done
-        BBMEAN=$M
-        BBVAR=$(echo "scale=10; $S/(${SAMPLES}-1.0)"  | bc)
-        BBSTDEV=$(echo "scale=10; sqrt(${BBVAR})"       | bc)
-        BBSTERR=$(echo "scale=10; ${BBSTDEV}/sqrt(${SAMPLES})" | bc)
-        echo "done"
-
-
-        echo -n "LAM map (${SAMPLES} Samples)............."
-        M=0;
-        S=0;
-        for k in `seq 1 ${SAMPLES}`;
-        do
-            x=`./${BINARY} ${DEV} ${N} ${R} 1`
-            oldM=$M;
-            M=$(echo "scale=10;  $M+($x-$M)/$k"           | bc)
-            S=$(echo "scale=10;  $S+($x-$M)*($x-${oldM})" | bc)
-        done
-        LAMMEAN=$M
-        LAMVAR=$(echo "scale=10; $S/(${SAMPLES}-1.0)"   | bc)
-        LAMSTDEV=$(echo "scale=10; sqrt(${LAMVAR})"      | bc)
-        LAMSTERR=$(echo "scale=10; ${LAMSTDEV}/sqrt(${SAMPLES})" | bc)
-        echo "done"
-
-        echo "B=${B} N=${N} --> BB ${BBMEAN} [ms] (${BBVAR}, ${BBSTDEV}, ${BBSTERR}) LAM ${LAMMEAN} [ms] (${LAMVAR}, ${LAMSTDEV}, ${LAMSTERR}"
-        echo "${N}   ${B}    ${BBMEAN}    ${BBVAR}    ${BBSTDEV}   ${BBSTERR}        ${LAMMEAN} ${LAMVAR} ${LAMSTDEV} ${LAMSTERR}" >> data/${OUTFILE}_B${B}.dat
-        echo " "
-    done 
+        echo " " >> "${OUTPUT}"
+        echo ""
+        echo ""
+        echo ""
+        cont=${cont}+1
+    done
     echo " "
-done 
+done
+echo "END TIME"
+timedatectl
+echo "*******************************"
+
+
+
+
